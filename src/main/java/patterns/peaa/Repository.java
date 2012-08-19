9e121b48bbfe45d75b48e033ca70112db829e2cc
Keith Webster Johnston <k.w.johnston@gmail.com>
2012-08-19 22:28:46 +0100

    Added the 'Repository' pattern.

---
+++ src/main/java/patterns/peaa/Repository.java
@@ -0,0 +1,46 @@
+/*-----------------------------------------------------------------------------
+ * Copyright © 2011 Keith Webster Johnston.
+ * All rights reserved.
+ *
+ * This file is part of Odelay.
+ *
+ * Odelay is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Odelay is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Odelay.  If not, see <http://www.gnu.org/licenses/>.
+ *---------------------------------------------------------------------------*/
+package patterns.peaa;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import com.johnstok.odelay.Pattern;
+
+
+/**
+ * The 'Repository' pattern.
+ *
+ * @author Keith Webster Johnston.
+ */
+@Pattern(
+    title="Repository",
+    references={
+        "http://martinfowler.com/eaaCatalog/repository.html"
+    }
+)
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface Repository {
+
+}
